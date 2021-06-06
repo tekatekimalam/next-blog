246246c6a889a7a26a2b3be91096a6ff1317f907
alamasyarie <maiaelarsya@gmail.com>
2021-06-06 15:02:52 +0700

    Make fixture data

---
+++ libs/data.js
@@ -0,0 +1,23 @@
+export const blogPosts = [
+  {
+    title: "My first post!",
+    slug: "first",
+    date: new Date(),
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.",
+  },
+  {
+    title: "My second post!",
+    slug: "second",
+    date: new Date(),
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.",
+  },
+  {
+    title: "My third post!",
+    slug: "third",
+    date: new Date(),
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque alias id maxime ipsum nemo tempore vitae ducimus! Magni, incidunt, doloribus quae quam ab corporis, molestiae et molestias quos saepe veniam.",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { blogPosts } from "../libs/data";
 
 export default function Home() {
   return (
@@ -12,6 +13,16 @@ export default function Home() {
       <main>
         <h1>My Blog</h1>
       </main>
+
+      <div>
+        {blogPosts.map(item => (
+          <div key={item.slug}>
+            <div>{item.title}</div>
+            <div>{item.date.toString()}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
