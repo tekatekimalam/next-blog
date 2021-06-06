34c1ca53e5e79b56172c72ab8468939aa993efbc
alamasyarie <maiaelarsya@gmail.com>
2021-06-06 15:44:48 +0700

    route dynamic for single blog page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+
 import { blogPosts } from "../libs/data";
 
 export default function Home() {
@@ -17,7 +19,11 @@ export default function Home() {
       <div>
         {blogPosts.map(item => (
           <div key={item.slug}>
-            <div>{item.title}</div>
+            <div>
+              <Link href={`/blog/${item.slug}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
             <div>{item.date.toString()}</div>
             <div>{item.content}</div>
           </div>
